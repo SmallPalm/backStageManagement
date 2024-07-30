ee003a155e15619b0d43afd158f8cef7c919004c
coderLi <17731035485@163.com>
2024-07-30 19:06:01 +0800

    配置esLint文件

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,13 +3,18 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting'
+    '@vue/eslint-config-prettier/skip-formatting',
+    'plugin:prettier/recommended'
   ],
   parserOptions: {
     ecmaVersion: 'latest'
+  },
+  // 自定义规则是否开启
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'off'
   }
 }
