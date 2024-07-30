507e2e75a80381bf4671fc2a592138c8e74b1bf2
coderLi <17731035485@163.com>
2024-07-30 19:46:13 +0800

    提交重置CSS样式文件

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue"
+import App from "./App.vue"
+//CSS文件
+import "normalize.css"
+import "@/assets/css/index.less"
 
-createApp(App).mount('#app')
+createApp(App).mount("#app")
